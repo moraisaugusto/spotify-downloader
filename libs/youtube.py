51092e6394925102a492ef2e92940b181d05eebc
Augusto Morais <aflavio@gmail.co>
2015-11-30 21:49:39 -0200

    removing some trash chars

--- libs/youtube.py
+++ libs/youtube.py
@@ -21,15 +21,15 @@ if (sys.version_info > (3,0)):
     raw_input = input
 #else:
 #    from urllib2 import urlopen
-#    from urllib import quote_plus as qp  
+#    from urllib import quote_plus as qp
 
 class Youtube():
     """ Youtube
-    
+
     This Class performs the download from Youtube and Write the ID3 tags on mp3.
-    
-    Table musics: 
-        flags: 
+
+    Table musics:
+        flags:
             0 - not downloaded
             1 - downloaded
             2 - synced
@@ -39,28 +39,28 @@ class Youtube():
 
     def getMusics(self, playlist):
         """ Download musics from a playlist """
-        
+
         conn = sqlite3.connect('data/spotify.db')
         cursor = conn.cursor()
-        
+
         sql = 'SELECT * FROM musics WHERE flag = 0 AND playlist = "' + playlist + '"'
         cursor.execute(sql)
         musics = cursor.fetchall()
-        
+
         if not musics:
             print('All Musics from "{0}" playlist are already downloaded'. format(playlist))
             # Creating playlist only for downloaded musics
             sql = 'SELECT * FROM musics WHERE flag = 1 AND playlist = "' + playlist + '"'
             cursor.execute(sql)
             musics = cursor.fetchall()
-            
-            self.create_m3u(playlist, musics)    
+
+            self.create_m3u(playlist, musics)
             exit()
-        
+
         print('Playlist: ' + str(musics[0][1]))
-        
+
         for music in musics:
-            
+
             # check if music is already downloaded
             downloaded = self.find(music[0] + '.mp3', self.DL_DIR)
             if downloaded is not None:
@@ -69,25 +69,25 @@ class Youtube():
                 cursor.execute(sql)
                 conn.commit()
                 continue
-            
+
             query = music[2] + " - " + music[3]
-            
+
             # Magic happens here.
             video_link = self.performYoutubeSearch(query)
-            
+
             if video_link is not False:
-                
+
                 self.performYoutubeDownload(music[1], music[0], video_link)
                 self.writeID3tag(music, music[0])
             else:
                 print ('Nothing found')
-                
-        self.create_m3u(playlist, musics)    
-        
+
+        self.create_m3u(playlist, musics)
+
         return True
-    
-        
-    
+
+
+
     @retry(3, delay=0.1)
     def performYoutubeSearch(self, query):
         """ Perform a Youtube Search """
@@ -99,9 +99,9 @@ class Youtube():
                 # May change when Youtube gets updated in the future.
                 video_link = link.get('href')
                 break
-        
+
         result = soup.find('a', 'yt-uix-tile-link')
-        
+
         if result is not None:
             title = soup.find('a', 'yt-uix-tile-link').text
             title = title.encode('latin-1', 'ignore').decode('utf-8', 'ignore')
@@ -109,55 +109,55 @@ class Youtube():
             return video_link
         else:
             return False
-        
+
     def performYoutubeDownload(self, playlist, audioFileName, video_link):
-        """ Performs music download from Youtube """         
+        """ Performs music download from Youtube """
         # Links are relative on page, making them absolute.
         video_link = 'http://www.youtube.com/' + video_link
-        command = 'youtube-dl -q -o "' + self.DL_DIR + audioFileName + '.%(ext)s" --extract-audio --audio-format mp3 --audio-quality 0 ' + video_link 
-        
+        command = 'youtube-dl -q -o "' + self.DL_DIR + audioFileName + '.%(ext)s" --extract-audio --audio-format mp3 --audio-quality 0 ' + video_link
+
         # Youtube-dl is a proof that god exists.
         os.system(command)
         return True
-        
+
     def writeID3tag(self, info, audioFile):
         """ Writes the ID3 on mp3  """
-        
+
         # writing id3 tag
         audioPath = self.DL_DIR + audioFile + '.mp3'
         audio = ID3(audioPath)
-        
+
         audio.add(TIT2(encoding=3, text=info[3])) # title
         audio.add(TPE1(encoding=3, text=info[2])) #artist
         audio.add(TALB(encoding=3, text=info[4])) # album
         audio.add(TRCK(encoding=3, text=str(info[5]))) # track Number
         audio.add(COMM(encoding=3, text=u'by <aflavio at gmail.com>')) # comments
-        
+
         # writing artwork on mp3
         artworkFile = audioFile + '.jpg'
         urllib.request.urlretrieve(info[6], self.TMP_DIR + artworkFile)
         pic = APIC(3, u'image/jpg', 3, u'Front cover', open(self.TMP_DIR + artworkFile, 'rb').read())
         audio.add(pic)
-        
+
         audio.save()
-    
+
     def create_m3u(self, playlist, musics):
         """ Create the playlist """
-        
+
         # converting list to array
         all_musics = []
         for music in musics:
             all_musics.append(music[0] + '.mp3')
-        
+
         try:
             print ('Processing directory...')
-    
+
             playlist = playlist + '.m3u'
             mp3s = []
-    
+
             os.chdir(self.DL_DIR)
-            
-            # getting mp3 data            
+
+            # getting mp3 data
             for file in all_musics:
                 meta_info = {
                     'filename': file,
@@ -165,27 +165,27 @@ class Youtube():
                     'tracknumber': EasyID3(file)['tracknumber'][0].split('/')[0],
                 }
                 mp3s.append(meta_info)
-            
+
             if len(mp3s) > 0:
                 print ('Writing playlist {0}...' . format(playlist))
-    
+
                 # write the playlist
                 of = open('../' + playlist, 'w')
                 of.write("#EXTM3U\n")
-    
+
                 #sorted by track number
                 for mp3 in mp3s:
                     of.write('#EXTINF:{0},{1}\n' . format(mp3['length'], mp3['filename']))
                     of.write('downloads/' + mp3['filename'] + "\n")
-    
+
                 of.close()
             else:
                 print ('No mp3 files found.')
-    
+
         except:
             print ('ERROR occured when processing directory. Ignoring.')
             print ('Text: {0}'. format(sys.exc_info()[0]))
-    
+
     def find(self, name, path):
         """ Try to find a file in a directory """
         for root, dirs, files in os.walk(path):
